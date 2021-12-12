ffa96d1748a70bcf05a9ff77694fe137a06266b1
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-12-12 19:35:32 -0400

    Rick and morty app, first 20 characters

---
+++ components/Character.js
@@ -0,0 +1,30 @@
+export const Character = ({
+  name,
+  status,
+  species,
+  location,
+  image,
+  gender,
+}) => {
+  return (
+    <div className='character'>
+      <img src={image} alt={name} className='character__img' />
+      <div className='character__info'>
+        <h2 className='character__name'>{name}</h2>
+        <p className='character__status'>
+          <span className={`character__status--${status.toLowerCase()}`}></span>{' '}
+          {`${status} - ${species}`}
+        </p>
+        <div className='character__episode'>
+          <p className='character__title'>Gender:</p>
+          <p className='character__description'>{gender}</p>
+        </div>
+
+        <div className='character__location'>
+          <p className='character__title'>Last known location:</p>
+          <p className='character__description'>{location.name}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
