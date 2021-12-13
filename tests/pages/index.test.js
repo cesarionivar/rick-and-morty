befbd95006f5d85c8866027f79f4f705c9513702
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-12-13 15:06:44 -0400

    Initial test

---
+++ tests/pages/index.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Home, { getStaticProps } from '../../pages/index';
+import { resultApi } from '../mocks/resultApi';
+
+describe('Test in Home page', () => {
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
+
+  it('should call rick and morty api', async () => {
+    fetch.mockResponseOnce(JSON.stringify(resultApi));
+
+    const response = await getStaticProps();
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          characters: resultApi,
+        },
+      })
+    );
+  });
+
+  it('renders the title of the page', () => {
+    render(<Home characters={resultApi} />);
+    expect(
+      screen.getByText('The Rick And Morty Characters')
+    ).toBeInTheDocument();
+  });
+});
